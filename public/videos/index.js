9dca26fc61e78762a96a7bc49f38f96592ab545d
greendream21 <uri.dev.0802@gmail.com>
2023-04-13 18:41:10 -0600

    CSS and Page Layouts

--- public/videos/index.js
+++ public/videos/index.js
@@ -8,7 +8,11 @@ import heroVideo from "./hero-video.mp4";
 import banner1 from "./banner-left.mp4";
 import banner2 from "./banner-left.mp4";
 import banner3 from "./banner-left.mp4";
-
+import coin1 from "./coin-1.mp4";
+import coin2 from "./coin-2.mp4";
+import coin3 from "./coin-3.mp4";
+import coin4 from "./coin-4.mp4";
+import coin5 from "./coin-5.mp4";
 
 
 
@@ -16,6 +20,11 @@ export default {
     banner1,
     banner2,
     banner3,
+    coin1,
+    coin2,
+    coin3,
+    coin4,
+    coin5,
     common,
     uncommon,
     rare,
