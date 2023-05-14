4b6b19e50b3f8ab9fa841138f2d1a2619d72749b
greendream21 <uri.dev.0802@gmail.com>
2023-05-14 06:12:21 +0000

    Update 4 files

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,6 +1,8 @@
 
 import React from "react";
 
+
+/* org code
 //INTERNAL IMPORT
 import Style from "./Button.module.css";
 
@@ -10,6 +12,31 @@ const Button = ({ btnName, handleClick, icon, classStyle }) => {
       <button
         className={`${Style.btn} ${classStyle}`}
         onClick={() => handleClick()}
+      >
+        {icon} {btnName}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
+*/
+
+//INTERNAL IMPORT
+import Style from "./Button.module.css";
+
+const Button = ({ btnName, handleClick, icon, classStyle }) => {
+  const handleButtonClick = () => {
+    if (handleClick) {
+      handleClick();
+    }
+  };
+
+  return (
+    <div className={Style.box}>
+      <button
+        className={`${Style.btn} ${classStyle}`}
+        onClick={handleButtonClick}
       >
         {icon} {btnName}
       </button>
