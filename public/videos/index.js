d6e02c8cd8495b5ccaf5e276a5868c98ed19e90e
greendream21 <uri.dev.0802@gmail.com>
2023-05-16 09:50:06 +0000

    Update 5 files

--- public/videos/index.js
+++ public/videos/index.js
@@ -4,7 +4,7 @@ import rare from "./rare.mp4";
 import epic from "./epic.mp4";
 import legendary from "./legendary.mp4";
 import bannerVideo from "./banner-video.mp4";
-import heroVideo from "./hero-video.mp4";
+import heroVideo from "./hero-video.webm";
 import banner1 from "./banner-left.mp4";
 import banner2 from "./banner-center.mp4";
 import banner3 from "./banner-right.mp4";
