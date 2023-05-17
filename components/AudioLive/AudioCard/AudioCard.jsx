f05fbd7b0c6fd68d4a55ebe4a0fa4134c0078228
greendream21 <uri.dev.0802@gmail.com>
2023-05-16 18:09:14 -0600

    Audio Card

--- components/AudioLive/AudioCard/AudioCard.jsx
+++ components/AudioLive/AudioCard/AudioCard.jsx
@@ -1,100 +1,179 @@
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import { TbPlayerPlay, TbPlayerPause } from "react-icons/tb";
-
-//INTERNAL IMPORT
+import { MdTimer } from "react-icons/md";
+import { Loader } from "../../../components/componentsindex";
 import Style from "./AudioCard.module.css";
+import Rating from "react-rating";
+import { FaRegStar, FaStar } from "react-icons/fa";
 import images from "../../../img";
-import LikeProfile from "../../LikeProfile/LikeProfile";
+import Image from "next/image";
 
-const AudioCard = () => {
-  const [like, setLike] = useState(false);
-  const [play, setPlay] = useState(false);
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
-  const likeNft = () => {
-    if (!like) {
-      setLike(true);
-    } else {
-      setLike(false);
-    }
-  };
 
-  const playMusic = () => {
-    if (!play) {
-      setPlay(true);
-    } else {
-      setPlay(false);
-    }
-  };
-  return (
-    <div className={Style.audioCard}>
-      <div className={Style.audioCard_box}>
-        <div className={Style.audioCard_box_like_time}>
-          <div className={Style.audioCard_box_like} onClick={() => likeNft()}>
-            {like ? (
-              <AiFillHeart className={Style.audioCard_box_like_icon} />
-            ) : (
-              <AiOutlineHeart
-                className={Style.audioCard_box_like_icon_unlike}
-              />
-            )}
+import { motion } from 'framer-motion';
 
-            <span>25</span>
-          </div>
+import Link from "next/link";
 
-          <div className={Style.audioCard_box_time}>
-            <div className={Style.audioCard_box_like_time_remaing}>
-              <small>TIME REMAINING</small>
-              <h5>3H : 6M : 9S</h5>
-            </div>
-          </div>
-        </div>
+const mp3Image = "mp3.jpg";
 
-        <div className={Style.audioCard_box_player}>
-          <Image src={images.musiceWave} alt="music" width={200} />
-          <div
-            className={Style.audioCard_box_musicPlayer}
-            onClick={() => playMusic()}
-          >
-            {play ? (
-              <div className={Style.audioCard_box_musicPlayer_icon}>
-                <TbPlayerPause />
-              </div>
-            ) : (
-              <div className={Style.audioCard_box_musicPlayer_icon}>
-                <TbPlayerPlay />
-              </div>
-            )}
-          </div>
-        </div>
+const AudioCard = ({ NFTData, likes }) => {
+  const [fileTypes, setFileTypes] = useState({});
+  const [loading, setLoading] = useState(true);
 
-        <div className={Style.audioCard_box_details}>
-          <div className={Style.audioCard_box_details_info}>
-            <h4>MUSIC NFT #1123</h4>
-            <div className={Style.audioCard_box_details_info_price}>
-              <small>PRICE</small>
-              <p>0.25 BNB</p>
-            </div>
-          </div>
+  useEffect(() => {
+    const fetchFileTypes = async () => {
+      const fileTypesObj = {};
 
-          <div className={Style.audioCard_box_details_stock}>
-            <LikeProfile />
-            <small>100 IN STOCK</small>
-          </div>
-        </div>
+      for (const el of NFTData) {
+        try {
+          const response = await fetch(el.image);
+          const contentType = response.headers.get("content-type");
+          fileTypesObj[el.image] = contentType;
+        } catch (error) {
+          console.log(error);
+        }
+      }
 
-        <div className={Style.audioCard_box_img}>
-          <Image
-            src={images.creatorbackground10}
-            alt="background"
-            width={525}
-            height={500}
-          />
-        </div>
-      </div>
+      setFileTypes(fileTypesObj);
+      setLoading(false);
+    };
+
+    fetchFileTypes();
+  }, [NFTData]);
+
+  const RenderImage = ({ src }) => (
+    <LazyLoadImage
+      src={src}
+      alt="NFT"
+      width={350}
+      height={350}
+      effect="blur"
+      className={Style.audioCard_box_img_img}
+    />
+  );
+
+  const RenderVideo = ({ src }) => (
+    <LazyLoadComponent>
+      <video
+        src={src}
+        alt="NFT"
+        width={350}
+        height={350}
+        objectFit="cover"
+        className={Style.audioCard_box_img_img}
+        controls
+        preload="auto"
+      />
+    </LazyLoadComponent>
+  );
+
+  const RenderAudio = ({ src }) => (
+    <div className={Style.audioCard_box_audio}>
+      <img
+        src={mp3Image}
+        alt="Default"
+        width={350}
+        height={350}
+        objectFit="cover"
+        className={Style.audioCard_box_img_audio}
+      />
+      <audio
+        src={src}
+        className={Style.audioCard_box_audio_controls}
+      />
     </div>
   );
+
+  const RenderDefault = () => (
+    <Image
+      src={images.invalidImage}
+      alt="NFT"
+      width={350}
+      height={300}
+      objectFit="cover"
+      className={Style.audioCard_box_img}
+      
+    />
+  );
+
+  const renderFilePreview = (el) => {
+    const fileType = fileTypes[el.image];
+
+    if (fileType && fileType.includes("image")) {
+      return <RenderImage src={el.image} />;
+    } else if (fileType && fileType.includes("video")) {
+      return <RenderVideo src={el.image} />;
+    } else if (fileType && fileType.includes("audio")) {
+      return <RenderAudio src={el.image} />;
+    } else {
+      return <RenderDefault />;
+    }
+  };
+  
+  
+  return (
+  <motion.div className={Style.audioCard_container}>
+    {loading ? (
+      <div className={Style.loading}>
+        <p className={`${Style["loading-message"]} ${Style["loading-message-animate"]}`}>
+          Loading NFTs...
+        </p>
+      </div>
+   ) : (
+    <div className={Style.audioCard}>
+      {NFTData.map((el, i) => (
+        <motion.div className={Style.audioCard_box} key={`${el.tokenId}-${i}`}>
+          <div className={Style.audioCard_box_img}>
+            {renderFilePreview(el)}
+          </div>
+          <Link
+                href={{ pathname: "/NFTDetails", query: el }}
+                key={`${el.tokenId}-${i}`}
+                >
+          <div className={Style.audioCard_box_content}>
+            <div className={Style.audioCard_box_content_box}>
+            <div className={Style.audioCard_box_title}>
+                   <p>NFT NAME:</p>
+                </div> 
+              <div className={Style.audioCard_box_title_name}>
+                   <p>{el.name}</p>
+                </div> 
+                <div className={Style.audioCard_box_token}>
+                  <p>NFT MINT ID</p>
+                </div>               
+                <div className={Style.audioCard_box_token}>
+                  <p># {el.tokenId}</p>
+                </div>
+                <div className={Style.audioCard_box_title_like}>
+                <Rating
+                  emptySymbol={<FaRegStar style={{ marginRight: "5px" }} />}
+                  fullSymbol={<FaStar style={{ marginRight: "5px" }} />}
+                  onClick={(value) => likeNFT(el.tokenId, value)}
+                  initialRating={likes[el.tokenId] ? likes[el.tokenId].rating : 0}
+                />
+                <div className={Style.likesNumber}>
+                  <span>{likes[el.tokenId] ? likes[el.tokenId].count : 0}</span>
+                </div>                
+              </div>              
+            </div>
+
+            <div className={Style.audioCard_box_price}>              
+              <div className={Style.audioCard_box_price_box}>
+                <small>CURRENT PRICE</small>
+                <p>{parseFloat(el.price) * 10 ** 9} BNB</p>
+              </div>
+            </div>
+
+          </div>
+          </Link>
+        </motion.div>
+      ))}
+    </div>
+  )}
+</motion.div>
+);
 };
 
 export default AudioCard;
--- components/AudioLive/AudioLive.jsx
+++ components/AudioLive/AudioLive.jsx
@@ -1,26 +1,111 @@
-import React from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-//INTERNAL IMPORT
 import Style from "./AudioLive.module.css";
-import AudioCard from "./AudioCard/AudioCard";
-import AudioCardSmall from "./AudioCardSmall/AudioCardSmall";
+import AudioCard from "../../components/AudioLive/AudioCard/AudioCard";
+import { NFTMarketplaceContext } from "../../Context/NFTMarketplaceContext";
 
-const AudioLive = () => {
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+
+const Slider = () => {
+  const { fetchNFTs, setError } = useContext(NFTMarketplaceContext);
+  const [nfts, setNfts] = useState([]);
+  const [fileTypes, setFileTypes] = useState({});
+  const [likes, setLikes] = useState({});
+
+  //SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const items = await fetchNFTs();
+        setNfts(items.reverse());
+      } catch (error) {
+        setError("Please reload the browser", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const [width, setWidth] = useState(0);
+  const dragSlider = useRef();
+
+
+
+  useEffect(() => {
+    setWidth(dragSlider.current.scrollWidth - dragSlider.current.offsetWidth);
+  });
+
+  const handleScroll = (direction) => {
+    const { current } = dragSlider;
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction == "left") {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+
+
+  useEffect(() => {
+    const fetchFileTypes = async () => {
+      const fileTypesObj = {};
+
+      for (const el of nfts) {
+        try {
+          const response = await fetch(el.image);
+          const contentType = response.headers.get("content-type");
+          fileTypesObj[el.image] = contentType;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      setFileTypes(fileTypesObj);
+    };
+
+    fetchFileTypes();
+  }, [nfts]);
+
+
+  const audioNFTs = nfts.filter(nft => fileTypes[nft.image] && fileTypes[nft.image].includes('audio'));
+
   return (
-    <div className={Style.audioLive}>
-      <div className={Style.audioLive_box}>
-        <div className={Style.audioLive_box_left}>
-          <AudioCard />
-          <AudioCard />
+    <div className={Style.audioLiveContainer}>
+      <div className={Style.audioLive}>
+        <div className={Style.audioLive_box}>
+          <div className={Style.audioLive_box_button}></div>
         </div>
-        <div className={Style.audioLive_box_right}>
-          <AudioCardSmall />
-          <AudioCardSmall />
-          <AudioCardSmall />
-        </div>
+        <Swiper
+          ref={dragSlider}
+          className={Style.audioLive_box_items}
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          spaceBetween={0}
+          slidesPerView={2}
+          loop={true}
+          navigation={true}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 5000 }}
+        >
+          {audioNFTs.map((nft) => (
+            <SwiperSlide key={nft.tokenId}>
+              <AudioCard NFTData={[nft]} likes={likes} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
 };
 
-export default AudioLive;
+export default Slider;
+
